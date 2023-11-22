4bab4d9d37e6dee7780f14da9a5c46d1f90acf19
Cesar-Andres-R <bocchi.the.rock.010@gmail.com>
2023-11-21 18:49:33 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Trabajos_2_Unidad_2.Clases;
+
+namespace Trabajos_2_Unidad_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicios ejercicios = new Ejercicios();
+            //ejercicios.TresArrays();
+
+            //ejercicios.ValorPosicion();
+
+            //ejercicios.IDsTresArreglos();
+
+            //ejercicios.MostrarNoRepetidos();
+
+            ejercicios.NotasPromedio();
+        }
+    }
+}
